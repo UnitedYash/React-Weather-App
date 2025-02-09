b75c192df847fa8cb73526ac9248474462a672ef
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-02-09 12:23:21 -0500

    main

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import './Weather.css'
+import React from 'react';
+import './Weather.css';
+import snow from "../assets/snow.png";
+
 const Weather = () => {
   return (
     <div className='weather'>
         <div className='search-bar'>
             <input type='text' placeholder='Enter City Name' />
-            
+            <button className='submit-button'>Search</button>
         </div>
+        <img src={snow} alt='snow' width="70px" height="70px" />
+        <p>20°C</p>
+        <p>Toronto</p>
     </div>
   )
 }
