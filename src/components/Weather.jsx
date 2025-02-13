50f54ba07d7e66ef61f4677ef01f56e6dc52cda6
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-02-13 10:11:02 -0500

    Made changes to the UI, mainly to css.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Weather.css';
 import snow from "../assets/snow.png";
-
+import windyImage from "../assets/windy.png";
+import humidityImage from "../assets/humidity.png";
 const Weather = () => {
   return (
     <div className='weather'>
@@ -10,8 +11,18 @@ const Weather = () => {
             <button className='submit-button'>Search</button>
         </div>
         <img src={snow} alt='snow' width="70px" height="70px" />
-        <p>20°C</p>
+        <p>1°C</p>
         <p>Toronto</p>
+        <div className='other-info'>
+          <div className="wind-speeds">
+            <img src={windyImage} width="50px" height="50px"></img>
+            <p>19km/h Wind Speed</p>
+          </div>
+          <div className="humidity">
+            <img src={humidityImage} width="50px" height="50px"></img>
+            <p>81% Humidity</p>
+          </div>
+        </div>
     </div>
   )
 }
