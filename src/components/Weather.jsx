a761dc4ca3578d6d2b8aa9659905709b92d0bd77
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-02-16 11:40:17 -0500

    Added api function call to any city, and updated dynammically

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 import './Weather.css';
 import snow from "../assets/snow.png";
 import windyImage from "../assets/windy.png";
 import humidityImage from "../assets/humidity.png";
+
+
+
 const Weather = () => {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("Toronto");
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid={apiKey}`;
+  const searchLocation = (event) => {
+    axios.get(URL).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+      setLocation("");
+
+    })
+  }
+
+
   return (
     <div className='weather'>
         <div className='search-bar'>
-            <input type='text' placeholder='Enter City Name' />
-            <button className='submit-button'>Search</button>
+        <input 
+            type="text" 
+            placeholder="Enter City Name" 
+            value={location} 
+            onChange={(e) => setLocation(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && searchLocation()}
+        />
+            <button className='submit-button' onClick={searchLocation}>Search</button>
         </div>
         <img src={snow} alt='snow' width="100px" height="100px" />
-        <p>1°C</p>
-        <p>Toronto</p>
+        <p>{data?.main?.temp} °C</p>
+        <p>{data.name}</p>
         <div className='other-info'>
           <div className="wind-speeds">
             <img src={windyImage} width="50px" height="50px"></img>
-            <p>19km/h Wind Speed</p>
+            <p>{Math.round(data?.wind?.speed * 3.6)} km/h Wind Speed</p>
           </div>
           <div className="humidity">
             <img src={humidityImage} width="50px" height="50px"></img>
-            <p>81% Humidity</p>
+            <p>{data?.main?.humidity}% Humidity</p>
           </div>
         </div>
     </div>
